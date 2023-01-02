c070a6ad9a7587187ad0aea57380748152be4cf8
Chris <cmoeller@mac.com>
2023-01-02 09:23:02 +0000

    Initial Commit

---
+++ BLEScanner/BLEScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEScannerApp.swift
+//  BLEScanner
+//
+//  Created by Christian Möller on 02.01.23.
+//
+
+import SwiftUI
+
+@main
+struct BLEScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
