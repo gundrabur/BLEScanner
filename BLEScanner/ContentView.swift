1a37ff62a3d3c1aee847aec4e0a274c300dce64b
Chris <cmoeller@mac.com>
2023-01-04 14:01:50 +0000

    first commit

--- BLEScanner/ContentView.swift
+++ BLEScanner/ContentView.swift
@@ -6,21 +6,61 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
+    @ObservedObject private var bluetoothScanner = BluetoothScanner()
+    @State private var searchText = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            HStack {
+                // Text field for entering search text
+                TextField("Search", text: $searchText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+                // Button for clearing search text
+                Button(action: {
+                    self.searchText = ""
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.secondary)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .opacity(searchText == "" ? 0 : 1)
+            }
+            .padding()
+
+            // List of discovered peripherals filtered by search text
+            List(bluetoothScanner.discoveredPeripherals.filter {
+                self.searchText.isEmpty ? true : $0.peripheral.name?.lowercased().contains(self.searchText.lowercased()) == true
+            }, id: \.peripheral.identifier) { discoveredPeripheral in
+                VStack(alignment: .leading) {
+                    Text(discoveredPeripheral.peripheral.name ?? "Unknown Device")
+                    Text(discoveredPeripheral.advertisedData)
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+            }
+
+            // Button for starting or stopping scanning
+            Button(action: {
+                if self.bluetoothScanner.isScanning {
+                    self.bluetoothScanner.stopScan()
+                } else {
+                    self.bluetoothScanner.startScan()
+                }
+            }) {
+                if bluetoothScanner.isScanning {
+                    Text("Stop Scanning")
+                } else {
+                    Text("Scan for Devices")
+                }
+            }
+            .padding()
+            .background(bluetoothScanner.isScanning ? Color.red : Color.blue)
+            .foregroundColor(Color.white)
+            .cornerRadius(5.0)
+        }
     }
 }
